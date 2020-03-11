5b8ff61b31a8dc0e7e013fa93303ab53b34c7d1c
Evgen-s <y.shadrin@gmail.com>
2020-03-12 00:19:35 +0300

    обновление проекта

--- source/js/script.js
+++ source/js/script.js
@@ -82,13 +82,88 @@ window.onload = function() {
       },
     })
   }
+
+   /* Слайдер Члены НОМИ */
+   if(document.querySelector('.members__council-container')) {
+    new Swiper ('.members__council-container', {
+      // Optional parameters
+      slidesPerView: 4,
+      spaceBetween: 30,
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+      }
+    })
+  }
+
+   if(document.querySelector('.members__container')) {
+      new Swiper ('.members__container', {
+        // Optional parameters
+        slidesPerView: 4,
+        spaceBetween: 30,
+
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'progressbar',
+        }
+      })
+    }
+
+
 }
 
+/* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
 
+( function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
 
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
 
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}( function( datepicker ) {
 
+datepicker.regional.ru = {
+	closeText: "Закрыть",
+	prevText: "&#x3C;Пред",
+	nextText: "След&#x3E;",
+	currentText: "Сегодня",
+	monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+	"Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+	monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+	"Июл","Авг","Сен","Окт","Ноя","Дек" ],
+	dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+	dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+	dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+	weekHeader: "Нед",
+	dateFormat: "dd.mm.yy",
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional.ru );
 
+return datepicker.regional.ru;
 
+} ) );
+var today = new Date();
+var y = today.getFullYear();
+$('#calendar').multiDatesPicker({
+  numberOfMonths: [1, 2],
+  maxPicks: 2
+});
 
+$('.reset1').on('click', function() {
+  $('#calendar').multiDatesPicker('resetDates');
+});
 
+$('.go').on('click', function() {
+  $('#calendar').multiDatesPicker('getDates');
+  $('.result').append($('#mdp-demo').multiDatesPicker('getDates'));
+});
